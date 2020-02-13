11a05808455c6af1042751ce36a4a48e05fe17c7
Manoj Singh <manojsinghrwt@hotmail.com>
2020-02-13 17:01:55 +0530

    Logic Change
    Now single post (any post type) is configure from admin to show alert box in particular single post

--- public/class-simple-alert-public.php
+++ public/class-simple-alert-public.php
@@ -2,35 +2,46 @@
 /**
  * The public-facing functionality of the plugin.
  */
-class Simple_Alert_Public {
+class Simple_Alert_Public{
 
 	public function __construct() {
 		add_action('wp_footer',array($this,'show_alert_box'));
 
 	}
 	public function show_alert_box(){
-		$is_posts 	=	get_option('simple_alert_is_posts');
-		$is_pages 	=	get_option('simple_alert_is_pages');
-		
-		$save_posts 	=	get_option('simple_alert_is_posts');
+		$post_id 	=	get_the_ID();
+		$save_post_types 	=	get_option('sa_post_types');
+		$save_single_post 	=	get_option('sa_single_post');
 
-		if(in_array('post',$save_posts)){
+		if(in_array('post',$save_post_types)){
 			if(is_single()){
-				Simple_Alert_Public::alert_box();
+				if($save_single_post){
+					if(in_array($post_id,$save_single_post)){
+						Simple_Alert_Public::alert_box();
+					}
+				}
 			}
 		}
-		if(in_array('page',$save_posts)){
+		if(in_array('page',$save_post_types)){
 			if(is_page()){
-				Simple_Alert_Public::alert_box();	
+				if($save_single_post){
+					if(in_array($post_id,$save_single_post)){
+						Simple_Alert_Public::alert_box();
+					}
+				}
 			}
 		}
 		$post_types = get_post_types( array('public'   => true,'_builtin' => false), 'names', 'and' );
 
 		if(!empty($post_types)){
 			foreach($post_types as $post_type){
-				if(in_array($post_type,$save_posts)){
+				if(in_array($post_type,$save_post_types)){
 					if(is_singular($post_type)){
-						Simple_Alert_Public::alert_box();	
+						if($save_single_post){
+							if(in_array($post_id,$save_single_post)){
+								Simple_Alert_Public::alert_box();
+							}
+						}
 					}
 				}	
 			}
