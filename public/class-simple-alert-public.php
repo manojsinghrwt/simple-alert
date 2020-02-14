0164f77740bbea5723965d3c51d16316fa8f493a
Manoj Singh <manojsinghrwt@hotmail.com>
2020-02-14 15:51:26 +0530

    language compatible
    language compatible added

--- public/class-simple-alert-public.php
+++ public/class-simple-alert-public.php
@@ -13,37 +13,45 @@ class Simple_Alert_Public{
 		$save_post_types 	=	get_option('sa_post_types');
 		$save_single_post 	=	get_option('sa_single_post');
 
-		if(in_array('post',$save_post_types)){
-			if(is_single()){
-				if($save_single_post){
-					if(in_array($post_id,$save_single_post)){
-						Simple_Alert_Public::alert_box();
-					}
-				}
-			}
-		}
-		if(in_array('page',$save_post_types)){
-			if(is_page()){
-				if($save_single_post){
-					if(in_array($post_id,$save_single_post)){
-						Simple_Alert_Public::alert_box();
-					}
-				}
-			}
-		}
-		$post_types = get_post_types( array('public'   => true,'_builtin' => false), 'names', 'and' );
-
-		if(!empty($post_types)){
-			foreach($post_types as $post_type){
-				if(in_array($post_type,$save_post_types)){
-					if(is_singular($post_type)){
-						if($save_single_post){
-							if(in_array($post_id,$save_single_post)){
-								Simple_Alert_Public::alert_box();
-							}
+		if($save_post_types){
+			if(in_array('post',$save_post_types)){
+				if(is_single() && is_singular('post')){
+					if($save_single_post){
+						if(in_array($post_id,$save_single_post)){
+							Simple_Alert_Public::alert_box();
 						}
 					}
-				}	
+				}
+			}	
+		}
+		
+		if($save_post_types){
+			if(in_array('page',$save_post_types)){
+				if(is_singular('page')){
+					if($save_single_post){
+						if(in_array($post_id,$save_single_post)){
+							Simple_Alert_Public::alert_box();
+						}
+					}
+				}
+			}	
+		}
+		
+		$post_types = get_post_types( array('public'   => true,'_builtin' => false), 'names', 'and' );
+		
+		if(!empty($post_types)){
+			foreach($post_types as $post_type){
+				if($save_post_types){
+					if(in_array($post_type,$save_post_types)){
+						if(is_singular($post_type)){
+							if($save_single_post){
+								if(in_array($post_id,$save_single_post)){
+									Simple_Alert_Public::alert_box();
+								}
+							}
+						}
+					}	
+				}
 			}
 		}
 	}
