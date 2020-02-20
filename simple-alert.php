1bbe97525a4d5509a05716fea88f552175377aeb
Manoj Singh <manojsinghrwt@hotmail.com>
2020-02-20 14:51:14 +0530

    js and css file added
    js and css file added

--- simple-alert.php
+++ simple-alert.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Simple Alert
  * Plugin URI:        #
  * Description:       This is a just simple alert box to show in posts and pages.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Manoj Singh
  * Author URI:        http://manojsinghrwt.wordpress.com/
  * License:           GPL-2.0+
@@ -27,21 +27,32 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * Currently plugin version.
  */
-define( 'SIMPLE_ALERT_VERSION', '1.0.0' );
+define( 'SIMPLE_ALERT_VERSION', '1.0.1' );
 
 /**
  * Begins execution of the plugin.
  */
 require plugin_dir_path( __FILE__ ) . 'admin/class-simple-alert-admin.php';
 require plugin_dir_path( __FILE__ ) . 'public/class-simple-alert-public.php';
-
+/**
+ * Register the Style for the admin area.
+ *
+ * @since    1.0.0
+ */
+function simple_alert_admin_styles() {
+	wp_register_style( 'simple_alert_admin_css', plugin_dir_url( __FILE__ ).'assets/css/simple-alert-admin.css', false, '1.0' );
+	wp_enqueue_style( 'simple_alert_admin_css' );
+}
+add_action( 'admin_enqueue_scripts', 'simple_alert_admin_styles' );
+/**
+ * Register the JavaScript for the admin area.
+ *
+ * @since    1.0.0
+ */
+function simple_alert_admin_scripts() {
+	wp_register_script( 'simple_alert_admin_js', plugin_dir_url( __FILE__ ).'assets/js/simple-alert-admin.js', false, '1.0', false );
+	wp_enqueue_script( 'simple_alert_admin_js' );
+}
+add_action( 'admin_enqueue_scripts', 'simple_alert_admin_scripts' );
 $simpleAlertAdmin = new Simple_Alert_Admin();
 $simpleAlertPublic = new Simple_Alert_Public();
-
-add_action( 'init', 'simple_alert_load_plugin_textdomain');
-function simple_alert_load_plugin_textdomain(){
-	$domain 	= 	'simple-alert';
-	$locale 	= 	apply_filters('plugin_locale',get_locale(),$domain);
-	load_textdomain($domain,trailingslashit(WP_LANG_DIR).$domain.'/'.$domain.'-'.$locale.'.mo');
-	load_plugin_textdomain( $domain, false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-}
